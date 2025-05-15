026989306137d997d09a6b6dc6ca16d12c44d28f
Jonah Benedicto <jfranc.ben@gmail.com>
2025-05-16 00:28:43 +1000

    Add two sum solution

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,5 @@ mod two_sum;
 use two_sum::Solution;
 
 fn main() {
-    let nums = vec![2,7,11,15];
-    let result = Solution::two_sum(nums, 9);
-    println!("{:?}", result); 
+    println!("{:?}", Solution::two_sum(vec![2,7,11,15], 9)); 
 }
