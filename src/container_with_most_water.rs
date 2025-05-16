0c9e810f7a772918c8f96ce4d6a77e9ee4f018b7
Jonah Benedicto <jfranc.ben@gmail.com>
2025-05-16 17:21:16 +1000

    Add container with most water solution

--- src/container_with_most_water.rs
+++ src/container_with_most_water.rs
@@ -4,20 +4,38 @@ pub struct Solution;
 impl Solution {
     pub fn max_area(height: Vec<i32>) -> i32 {
         // Brute force solution // Not a solution on leetcode
+        // let mut max_area = 0;
+        // for (i, h1) in height.iter().enumerate() {
+        //     for (j, h2) in height.iter().enumerate() {
+        //         if i == j {
+        //             continue;
+        //         }
+        //         if h1 <= h2 && h1 * (j as i32 - i as i32) > max_area {
+        //             max_area = h1 * (j as i32 - i as i32);
+        //         } else if h2 < h1 && h2 * (j as i32 - i as i32) > max_area {
+        //             max_area = h2 * (j as i32 - i as i32);
+        //         }
+        //     }
+        // }
+        // max_area
+        
+        // Sliding window solution
+        let mut i = 0;
+        let mut j = height.len() - 1;
         let mut max_area = 0;
-        for (i, h1) in height.iter().enumerate() {
-            for (j, h2) in height.iter().enumerate() {
-                if i == j {
-                    continue;
+        while i < j {
+            if &height[i] <= &height[j] {
+                if &height[i] * (j as i32 - i as i32) > max_area {
+                    max_area = &height[i] * (j as i32 - i as i32);
                 }
-                if h1 <= h2 && h1 * (j as i32 - i as i32) > max_area {
-                    max_area = h1 * (j as i32 - i as i32);
-                } else if h2 < h1 && h2 * (j as i32 - i as i32) > max_area {
-                    max_area = h2 * (j as i32 - i as i32);
+                i += 1;
+            } else if &height[j] < &height[i] {
+                if &height[j] * (j as i32 - i as i32) > max_area {
+                    max_area = &height[j] * (j as i32 - i as i32);
                 }
+                j -= 1;
             }
         }
         max_area
-        // 
     }
 }
