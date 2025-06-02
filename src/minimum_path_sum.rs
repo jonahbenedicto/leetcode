1c21e03e768d639b8454d87b293db586fbc87115
Jonah Benedicto <jfranc.ben@gmail.com>
2025-06-02 15:39:10 +1000

    64. Minimum Path Sum

---
+++ src/minimum_path_sum.rs
@@ -0,0 +1,23 @@
+// 64. Minimum Path Sum
+impl Solution {
+    pub fn min_path_sum(mut grid: Vec<Vec<i32>>) -> i32 {
+        let m = grid.len();
+        let n = grid[0].len();
+        
+        for j in 1..n {
+            grid[0][j] += grid[0][j - 1];
+        }
+        
+        for i in 1..m {
+            grid[i][0] += grid[i - 1][0];
+        }
+        
+        for i in 1..m {
+            for j in 1..n {
+                grid[i][j] += grid[i - 1][j].min(grid[i][j - 1]);
+            }
+        }
+        
+        grid[m - 1][n - 1]
+    }
+}
