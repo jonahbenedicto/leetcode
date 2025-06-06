3cdafff60b0f334b6dae9bace43cc95e6c7b18d0
Jonah Benedicto <jfranc.ben@gmail.com>
2025-06-06 11:52:30 +1000

    122. Best Time to Buy and Sell Stock II

---
+++ src/best_time_to_buy_and_sell_stock_II.rs
@@ -0,0 +1,14 @@
+// 122. Best Time to Buy and Sell Stock II
+impl Solution {
+    pub fn max_profit(prices: Vec<i32>) -> i32 {
+        let mut profit = 0;
+        
+        for i in 1..prices.len() {
+            if prices[i] > prices[i - 1] {
+                profit += prices[i] - prices[i - 1];
+            }
+        }
+        
+        profit
+    }
+}
